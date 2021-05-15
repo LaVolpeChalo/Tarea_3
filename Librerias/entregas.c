1b3abc3e578bf66501c0427c2c96a9ad34b537de
OkitaDev <soujiok@gmail.com>
2021-05-14 21:45:33 -0400

    Importar archivo casi terminado, falta elegir la estructura de datos a usar

--- Librerias/entregas.c
+++ Librerias/entregas.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "Interfaz\interfaz.h"
+
+typedef struct tipoEntrega
+{
+	int identificacion;
+	long long coordenadaX;
+	long long coordenadaY;
+}tipoEntrega;
+
+tipoEntrega * lecturaDeInformacion(char * lineaLeida, int id)
+{
+	tipoEntrega * nuevaPosicion = malloc (sizeof(tipoEntrega));
+	char * fragmento;
+
+	//Posicion X
+	fragmento = strtok(lineaLeida, " ");
+	nuevaPosicion->coordenadaX = strtol(fragmento, NULL, 10);
+
+	//Posicion Y
+	fragmento = strtok(NULL, "\n");
+	nuevaPosicion->coordenadaY = strtol(fragmento, NULL, 10);
+
+	//ID
+	nuevaPosicion->identificacion = id;
+	return nuevaPosicion;
+}
 
 void importarArchivo()
 {
@@ -24,7 +51,6 @@ void importarArchivo()
 	printf("\nIngrese la cantidad de lineas a leer: ");
 	scanf("%i", &cantLineas);
 
-
 	if(cantLineas == 0)
 	{
 		printf(red "\nNo se leyo ninguna linea\n" reset);
@@ -36,6 +62,7 @@ void importarArchivo()
 	
 	while(cont != cantLineas && fgets(lineaLeida, 100, archivo) != NULL)
 	{
+		tipoEntrega * nuevaPosicion = lecturaDeInformacion(lineaLeida, cont + 1);
 		cont++;
 	}
 
@@ -45,5 +72,7 @@ void importarArchivo()
 		printf("Se leyeron unicamente %i lineas\n"reset, cont);
 	}
 	
+
+
 	printf(green "\nEl archivo se importo correctamente!\n" reset);
 }
